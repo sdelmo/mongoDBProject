24e6f6199337622994a4ad0bb61d149956c33d95
Sebastian Delgado <sebastiansdelgado@gmail.com>
2023-12-31 14:25:11 +0000

    add list books and findbook endpoints

--- pymongo-fastapi-crud/routes.py
+++ pymongo-fastapi-crud/routes.py
@@ -6,6 +6,26 @@ from models import Book, BookUpdate
 
 router = APIRouter()
 
+
 @router.post("/", response_description="Create a new book", status_code=status.HTTP_201_CREATED, response_model=Book)
 def create_book(request: Request, book: Book = Body(...)):
-    
+    book = jsonable_encoder(book)
+    new_book = request.app.database["books"].insert_one(book)
+    created_book = request.app.database["books"].find_one(
+        {"_id": new_book.inserted_id})
+
+    return created_book
+
+
+@router.get("/", response_description="List all books", response_model=List[Book])
+def list_books(request: Request):
+    books = list(request.app.database["books"].find(limit=100))
+    return books
+
+
+@router.get("/{id}", response_description="Get a single book by id", response_model=Book)
+def find_book(id: str, request: Request):
+    if (book := request.app.database["books"].find_one({"_id": id})) is not None:
+        return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"Book with ID {id} not found")
