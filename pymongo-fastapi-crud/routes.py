67ec9d6d38f87135c8f7fcce09dd2388562e182b
Sebastian Delgado <sebastiansdelgado@gmail.com>
2024-01-03 20:17:34 +0000

    add endpoint for deleting books

--- pymongo-fastapi-crud/routes.py
+++ pymongo-fastapi-crud/routes.py
@@ -51,3 +51,15 @@ def update_book(id: str, request: Request, book: BookUpdate = Body(...)):
 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                         detail=f"Book with ID {id} not found")
+
+
+@router.delete("/id", response_description="Delete a book")
+def delete_book(id: str, request: Request, response: Response):
+    delete_result = request.app.database["books"].delete_one({"_id": id})
+
+    if delete_result.deleted_count == 1:
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return response
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"Book with ID {id} not found")
